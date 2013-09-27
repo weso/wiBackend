0c42bf7042d6654852fca52e66a2059dde06ab9b
Juan Castro Fernández <castrofernandez@gmail.com>
2013-09-27 12:31:13 +0200

    Application.java changes

--- wiLodPortalPlay/app/controllers/Application.java
+++ wiLodPortalPlay/app/controllers/Application.java
@@ -1,24 +1,15 @@
 package controllers;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 
-import play.api.templates.Html;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 import views.html.chart;
 
 import com.google.gson.Gson;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import es.weso.business.ComponentManagement;
 import es.weso.business.CountryGroupManagement;
@@ -44,7 +35,6 @@ import es.weso.wirouter.year.YearExpr;
 public class Application extends Controller {
 
 	private static Gson gson = new Gson();
-	private static XStream xstream = new XStream(new DomDriver());
 	private static ComponentManagement componentManager = ComponentManager
 			.getInstance();
 	private static CountryGroupManagement countryGroupManager = CountryGroupManager
@@ -62,79 +52,15 @@ public class Application extends Controller {
 	private static WeightSchemaManagement weightSchemaManager = WeightSchemaManager
 			.getInstance();
 
-<<<<<<< HEAD
 	private static Result render(Object obj) {
-		if (request().accepts("text/html"))
+		if (request().accepts("text/html")) 
 			return ok(chart.render("Chart Generator", obj));			
 		
 		return renderJSON(obj);
-=======
-	private static Result render(Object obj, Html html) {
-		if (request().accepts("text/html")) {
-			return ok(html);
-		}
-		if (request().accepts("text/turtle")) {
-			return renderTTL();
-		}
-		if (request().accepts("application/rdf+xml")) {
-			return renderRDF();
-		}
-		if (request().accepts("application/json")) {
-			return renderJSON(obj);
-		}
-		if (request().accepts("application/xml")) {
-			return renderXML(obj);
-		}
-		return badRequest(); // Should return 415
-	}
-
-	private static Result renderXML(Object obj) {
-		return ok(xstream.toXML(obj));
->>>>>>> 1ce63ed56ac603344e00c9980319354bc871721e
 	}
 
 	private static Result renderJSON(Object obj) {
 		return ok(gson.toJson(obj));
-	}
-
-	private static Result renderTTL() {
-		try {
-			return renderLD("text");
-		} catch (Exception e) {
-			return internalServerError();
-		}
-	}
-
-	private static Result renderRDF() {
-		try {
-			return renderLD("xml");
-		} catch (Exception e) {
-			return internalServerError();
-		}
-	}
-
-	private static Result renderLD(String output) throws Exception {
-		// FIXME This is fuseki dependant and dirty...
-		System.out.println(request().path());
-		String relativeUri = URLEncoder.encode(
-				request().path().substring(request().path().indexOf('/', 0)),
-				"UTF-8");
-		String url = "http://localhost:3030/computex/query?query=CONSTRUCT%7B%3Chttp%3A%2F%2Fdata.webfoundation.org%2Fwebindex%2Fv2013"
-				+ relativeUri
-				+ "%3E+%3Fp+%3Fl%7DWHERE%7B%3Chttp%3A%2F%2Fdata.webfoundation.org%2Fwebindex%2Fv2013"
-				+ relativeUri + "%3E+%3Fp+%3Fl%7D&output=" + output;
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				con.getInputStream()));
-		String inputLine;
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		while ((inputLine = in.readLine()) != null) {
-			baos.write(inputLine.getBytes());
-			baos.write("\n".getBytes());
-		}
-		in.close();
-		return ok(baos.toString());
 	}
 
 	public static Result index() {
@@ -142,84 +68,81 @@ public class Application extends Controller {
 	}
 
 	public static Result components() {
-		return render(componentManager.getAll(), null);
+		return render(componentManager.getAll());
 	}
 
 	public static Result component(String id) {
-		return render(componentManager.getOne(id), null);
+		return render(componentManager.getOne(id));
 	}
 
 	public static Result countries() {
-		return render(countryManager.getAllCountries(), null);
+		return render(countryManager.getAllCountries());
 	}
 
 	public static Result country(String code) {
-		return render(countryManager.getCountry(code), null);
+		return render(countryManager.getCountry(code));
 	}
 
 	public static Result datasets() {
-		return render(datasetManager.getAll(), null);
+		return render(datasetManager.getAll());
 	}
 
 	public static Result dataset(String id) {
-		return render(datasetManager.getOne(id), null);
+		return render(datasetManager.getOne(id));
 	}
 
 	public static Result indicators() {
-		return render(indicatorManager.getAllIndicators(), null);
+		return render(indicatorManager.getAllIndicators());
 	}
 
 	public static Result indicator(String id) {
-		return render(indicatorManager.getIndicatorByURI(id), null);
+		return render(indicatorManager.getIndicatorByURI(id));
 	}
 
 	public static Result observations() {
-		return render(observationManager.getAllObservations(), null);
+		return render(observationManager.getAllObservations());
 	}
 
 	public static Result observation(String id) {
-		return render(observationManager.getObservationByURI(id), null);
+		return render(observationManager.getObservationByURI(id));
 	}
 
 	public static Result observationByCountry(String country, int year,
 			String indicator) {
-		// TODO Redirect
-		return render(
-				observationManager.getAllObservationsByCountries(
-						Collections.singleton(country),
-						Collections.singleton(indicator),
-						Collections.singleton(year)), null);
+		return render(observationManager.getAllObservationsByCountries(
+				Collections.singleton(country),
+				Collections.singleton(indicator), Collections.singleton(year)));
 	}
 
 	public static Result regions() {
-		return render(countryGroupManager.getAllCountryGroups(), null);
+		return render(countryGroupManager.getAllCountryGroups());
 	}
 
 	public static Result region(String name) {
-		return render(countryGroupManager.getCountryGroup(name), null);
+		return render(countryGroupManager.getCountryGroup(name));
 	}
 
 	public static Result subindexes() {
-		return render(subindexManager.getAll(), null);
+		return render(subindexManager.getAll());
 	}
 
 	public static Result subindex(String id) {
-		return render(subindexManager.getOne(id), null);
+		return render(subindexManager.getOne(id));
 	}
 
 	public static Result weightSchemas() {
-		return render(weightSchemaManager.getAll(), null);
+		return render(weightSchemaManager.getAll());
 	}
 
 	public static Result weightSchema(String id) {
-		return render(weightSchemaManager.getOne(id), null);
+		return render(weightSchemaManager.getOne(id));
 	}
 
 	public static Result compare(String countries, String years,
 			String indicators) {
 		return render(observationManager.getAllObservationsByCountries(
 				parseCountryCodes(countries), parseObservations(indicators),
-				parseYears(years)), null);
+				parseYears(years)));
 	}
 
 	/**
