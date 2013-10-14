a08365c2f45c43941a0e6a5a4914870edc8685f6
César Luis Alvargonzález <info@cesarla.com>
2013-10-14 12:56:36 +0200

    Updates start.sh in order to load wiLodPortal as a Play-Framework application

--- start.sh
+++ start.sh
@@ -1,23 +1,27 @@
 #vars
 GIT_DIR="computex"
-FUSEKI_DIR="fuseki"
 GIT_REPO="https://github.com/weso/computex.git"
-PLAY_DIR="play" 
-MVN_DIR="mvn" 
+GIT_BRANCH="web"
+FUSEKI_DIR="fuseki"
+PLAY_DIR="wiLodPortal" 
 
 #Fetch the latest computex repository:
 if [[ -d "${GIT_DIR}" && ! -L "${GIT_DIR}" ]] ; then
-	cd ${GIT_DIR}
-	git pull origin master
-	cd ..
+	(cd ${GIT_DIR}; git checkout ${GIT_BRANCH})
+	(cd ${GIT_DIR}; git pull origin ${GIT_BRANCH})
 else
 	git clone ${GIT_REPO}
+	(cd ${GIT_DIR}; git checkout ${GIT_BRANCH})
 fi
 
 #Set FUSEKI_HOME Environment Variable
 export FUSEKI_HOME="${FUSEKI_HOME:-${FUSEKI_DIR}}"
-#Starts Fuseki:
-${FUSEKI_DIR}/fuseki-server --config=${FUSEKI_DIR}/ConfigFile.ttl & memcached & cd wiLodPortal; mvn tomcat7:undeploy; mvn tomcat7:deploy
 
-#Starts Play Project [Uncomment]
-#(cd ${PLAY_DIR; play run)
+#Starts Fuseki:
+${FUSEKI_DIR}/fuseki-server --config=${FUSEKI_DIR}/ConfigFile.ttl;
+
+# Starts Memchahed
+# memcached
+
+#Starts Play Project
+(cd ${PLAY_DIR}; play ~run)
